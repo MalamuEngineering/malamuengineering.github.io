63f08c49b444b32045dc8899874b9ac08bcf5569
PaulcyMboungou <paulcy.mbougou@gmail.com>
2019-12-21 12:08:58 +0530

    Paulcy added KSC Web App

---
+++ 01/assets/js/chatra.js
@@ -0,0 +1,15 @@
+
+<!-- Chatra {literal} -->
+
+(function(d, w, c) {
+    w.ChatraID = 'MLinng5uc6T5E6mTs';
+    var s = d.createElement('script');
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
+
+<!-- /Chatra {/literal} -->
